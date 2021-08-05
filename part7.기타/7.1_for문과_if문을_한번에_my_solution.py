043a00625b77f40b3a5d73ca946698e8b994542e
octo <dlqhdgkr456@gmail.com>
2021-08-05 23:24:55 +0900

    for문과 if문을 한번에 - list comprehension의 if문

---
+++ part7.기타/7.1_for문과_if문을_한번에_my_solution.py
@@ -0,0 +1,26 @@
+'''
+정수를 담은 리스트 mylist를 입력받아, 
+이 리스트의 원소 중 짝수인 값만을 제곱해 담은 새 리스트를 리턴하는 solution함수
+
+예를 들어, [3, 2, 6, 7]이 주어진 경우
+    3은 홀수이므로 무시합니다.
+    2는 짝수이므로 제곱합니다.
+    6은 짝수이므로 제곱합니다.
+    7은 홀수이므로 무시합니다.
+따라서 2의 제곱과 6의 제곱을 담은 리스트인 [4, 36]을 리턴해야합니다.
+'''
+
+
+def solution(mylist):
+    answer = []
+
+    for i in mylist:
+        if i % 2 == 0:
+            answer.append(i ** 2)
+    return answer
+
+
+my_list = [3, 2, 6, 7]
+print(my_list)
+
+print(solution(my_list))
